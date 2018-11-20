1d87426a75ae7969bdc7d418116515b5a48e9d11
hua.xu <1013299500@qq.com>
2018-11-20 22:33:43 +0800

    feature: container with most water

---
+++ code/source/ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
